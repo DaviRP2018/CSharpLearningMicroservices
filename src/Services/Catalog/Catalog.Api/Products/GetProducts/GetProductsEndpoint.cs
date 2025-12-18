ca0affa2c37f588274a592896fd26278e62b1fb1
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2025-12-18 00:31:31 -0300

    feat: implement GetProducts functionality with CQRS and Carter integration
    Added `GetProductsQuery` and its handler to retrieve product data using the CQRS pattern. Introduced `GetProductsEndpoint` with Carter for lightweight HTTP routing. Updated CQRS interfaces for cleaner syntax. Included a new test GET route in `api.http`.

---
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.Api.Products.GetProducts;
+
+// For best practice we define both Request and Response, although we don't need the Request here
+// public record GetProductsRequest;
+public record GetProductsResponse(
+    IEnumerable<Product> Products
+);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProduct")
+            .Produces<GetProductsResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
+}
