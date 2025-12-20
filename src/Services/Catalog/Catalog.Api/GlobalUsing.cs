5dc4fc11670a894de99646bd8e37b32cae36f4ba
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2025-12-20 18:55:49 -0300

    feat: integrate FluentValidation into Catalog API
    Added `FluentValidation.DependencyInjectionExtensions` to `BuildingBlocks`. Registered validators in `Catalog.Api` and updated dependencies and solution structure to support validation.

--- src/Services/Catalog/Catalog.Api/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.Api/GlobalUsing.cs
@@ -2,6 +2,7 @@
 global using Carter;
 global using Catalog.Api.Exceptions;
 global using Catalog.Api.Models;
+global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using MediatR;
