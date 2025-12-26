e2bca1d40f6330b214b45d6a5a0d0a77274f26a4
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2025-12-26 03:20:21 -0300

    feat: add global exception handling to Basket.API
    Registered `CustomExceptionHandler` and configured exception handling middleware in `Program.cs` to standardize error handling across the API.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -16,10 +16,12 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
