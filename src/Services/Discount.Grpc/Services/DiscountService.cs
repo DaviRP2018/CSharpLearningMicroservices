5ef6a56751314b1eba8e7c43caf649cfe89b0aeb
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2025-12-30 16:29:50 -0300

    feat: implement `DeleteDiscount` with database operations and error handling
    Enhanced the `DeleteDiscount` method in `DiscountService` to support database integration using Entity Framework Core. Added validation to handle non-existent discounts gracefully and incorporated logging for successful executions.

--- src/Services/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount.Grpc/Services/DiscountService.cs
@@ -65,9 +65,23 @@ public class DiscountService(DiscountContext dbContext, ILogger<DiscountService>
         return couponModel;
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest
+            request,
         ServerCallContext context)
     {
-        return base.DeleteDiscount(request, context);
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount not found for "
+                + $"{request.ProductName}"));
+
+        dbContext.Coupons.Remove(coupon);
+        await dbContext.SaveChangesAsync();
+        logger.LogInformation("Discount deleted successfully for {ProductName}",
+            request.ProductName);
+
+        return new DeleteDiscountResponse { Success = true };
     }
 }
