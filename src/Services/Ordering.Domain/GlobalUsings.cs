51b3ee1552bc9a141e2bb3f8a3905d59453fa2b8
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2025-12-31 18:30:09 -0300

    feat: implement core Ordering domain models and enable global usings
    Added domain models (`Order`, `OrderItem`, `Customer`, `Product`) and value objects (`Address`, `Payment`) to represent and enforce the business rules of the Ordering service. Introduced global usings for improved code simplicity and consistency. Defined `OrderStatus` enum to manage order lifecycle states.

---
+++ src/Services/Ordering.Domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using MediatR;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.ValueObjects;
---
+++ src/Services/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string? CardName { get; } = null!;
+    public string CardNumber { get; } = null!;
+    public string Expiration { get; } = null!;
+    public string Cvv { get; } = null!;
+    public int PaymentMethod { get; } = 0;
+}
