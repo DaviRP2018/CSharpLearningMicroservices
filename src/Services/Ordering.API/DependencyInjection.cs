89884f076e72d2e6eab845130f50a53dd20aa1fa
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2025-12-31 15:47:41 -0300

    feat: implement service registration for Ordering layers and remove unused Domain class
    Added `DependencyInjection` extensions to Ordering.Application, Ordering.Infrastructure, and Ordering.API for streamlined service registration. Updated `Program.cs` to integrate these layers. Removed unused `Class1` from Ordering.Domain.

---
+++ src/Services/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // TODO: Add Carter
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // TODO: mapCarter
+
+        return app;
+    }
+}
--- src/Services/Ordering.API/Program.cs
+++ src/Services/Ordering.API/Program.cs
@@ -1,6 +1,21 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+// -------------------
+// Infrastructure - EF Core
+// Application - MediatR
+// API - Carter, Health checks, ...
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+// -------------------
 
 var app = builder.Build();
 
