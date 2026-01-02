b41cf37e2cc809fdb29c6931a51e6554fab436dd
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-02 20:36:15 -0300

    feat: add `ApplicationDbContext` and configure Entity Framework Core in Ordering.Infrastructure
    Introduced `ApplicationDbContext` to manage database context for the Ordering service. Added EF Core dependency to enable data access and applied table configurations from the assembly.

---
+++ src/Services/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Common way to configure tables, but can get out of hand
+        // modelBuilder.Entity<Customer>().Property(c => c.Name).IsRequired().HasMaxLength(100);
+        // Way to group tables, it scans all IClasses to configure them
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
