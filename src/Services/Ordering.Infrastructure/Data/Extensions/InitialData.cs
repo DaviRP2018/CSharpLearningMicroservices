1cde8ea6aefb191f84987717e2bdfa179e670b32
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-04 01:18:53 -0300

    feat: add database initialization and seeding support for Ordering service
    Introduced `DatabaseExtensions` to automate database migration and initial data seeding during application startup. Enabled `InitializeDatabaseAsync` in the program pipeline to seed sample customer data, improving development and testing workflows.

---
+++ src/Services/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions;
+
+public class InitialData
+{
+    public static IEnumerable<Customer> Customers =>
+        new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(new Guid("9dbc33d1-2398-4b96-adc4-2752846e5b90")),
+                "harry", "harry@gemail.com.br"),
+            Customer.Create(CustomerId.Of(new Guid("68a41639-2658-4839-a201-aa1968ae8e3e")),
+                "john",
+                "john@gemail.com.br")
+        };
+}
