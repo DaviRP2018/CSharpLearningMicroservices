70eac7e57996d6c975c41ea18bd192d0d0aad99d
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-04 18:34:47 -0300

    feat: add event handlers and query for order management
    Implemented `OrderCreatedEventHandler` and `OrderUpdatedEventHandler` to log domain event handling. Introduced `GetOrdersByName` query and handler to fetch orders by name with DTO mapping.

---
+++ src/Services/Ordering.Application/Orders/Queries/GetOrdersByNameHandler.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+
+namespace Ordering.Application.Orders.Queries;
+
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken
+        cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.OrderName.Value.Contains(query.Name))
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        var orderDtos = ProjectToOrderDto(orders);
+
+        return new GetOrdersByNameResult(orderDtos);
+    }
+
+    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
+    {
+        List<OrderDto> result = new();
+        foreach (var order in orders)
+        {
+            var orderDto = new OrderDto(
+                order.Id.Value,
+                order.CustomerId.Value,
+                order.OrderName.Value,
+                new AddressDto(
+                    order.ShippingAddress.FirstName,
+                    order.ShippingAddress.LastName,
+                    order.ShippingAddress.EmailAddress,
+                    order.ShippingAddress.AddressLine,
+                    order.ShippingAddress.Country,
+                    order.ShippingAddress.State,
+                    order.ShippingAddress.ZipCode
+                ),
+                new AddressDto(
+                    order.BillingAddress.FirstName,
+                    order.BillingAddress.LastName,
+                    order.BillingAddress.EmailAddress,
+                    order.BillingAddress.AddressLine,
+                    order.BillingAddress.Country,
+                    order.BillingAddress.State,
+                    order.BillingAddress.ZipCode
+                ),
+                new PaymentDto(
+                    order.Payment.CardName,
+                    order.Payment.CardNumber,
+                    order.Payment.Expiration,
+                    order.Payment.Cvv,
+                    order.Payment.PaymentMethod
+                ),
+                order.Status,
+                order.OrderItems.Select(oi => new OrderItemDto(
+                    oi.OrderId.Value,
+                    oi.ProductId.Value,
+                    oi.Quantity,
+                    oi.Price
+                )).ToList()
+            );
+
+            result.Add(orderDto);
+        }
+
+        return result;
+    }
+}
