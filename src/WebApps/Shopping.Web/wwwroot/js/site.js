4f0d5b38c23580a2a1a0707e00e4e8dfa3e4cc6d
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-06 19:47:14 -0300

    feat: revamp static assets and update site layout for enhanced UX
    Replaced `site.css` with a new theme-specific `style.css` for improved design. Updated `_Layout.cshtml` with a modern navigation bar, search functionality, and footer enhancements. Added JavaScript for custom input handling and validation. Included new banner and product images to enrich site visuals.

--- src/WebApps/Shopping.Web/wwwroot/js/site.js
+++ src/WebApps/Shopping.Web/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    const quantity = 1;
 
-// Write your JavaScript code.
+    $('.quantity-right-plus').click(function (e) {
+        e.preventDefault();
+        let quantity = parseInt($('#quantity').val());
+        $('#quantity').val(quantity + 1);
+    });
+
+    $('.quantity-left-minus').click(function (e) {
+        e.preventDefault();
+        let quantity = parseInt($('#quantity').val());
+        if (quantity > 1) {
+            $('#quantity').val(quantity - 1);
+        }
+    });
+
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.getElementsByClassName('needs-validation');
+
+        // Loop over them and prevent submission
+        const validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
