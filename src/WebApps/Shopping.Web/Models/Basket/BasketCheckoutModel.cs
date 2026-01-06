67d7966db2214ac87028d1d6b256b5c9cf9836e8
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-06 18:16:20 -0300

    feat: add models for Catalog, Basket, and Checkout in Shopping.Web
    Introduced `ProductModel`, `ShoppingCartModel`, and `BasketCheckoutModel` classes with related wrapper records for consistent data representation in Catalog, Basket, and Checkout domains. Models support serialization and API response structure.

---
+++ src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class BasketCheckoutModel
+{
+    public string UserName { get; set; } = null!;
+    public Guid CustomerId { get; set; } = Guid.Empty;
+    public decimal TotalPrice { get; set; } = 0;
+
+    // Shipping and billing addresses
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string EmailAddress { get; set; } = null!;
+    public string AddressLine { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string State { get; set; } = null!;
+    public string ZipCode { get; set; } = null!;
+
+    // Payment
+    public string CardName { get; set; } = null!;
+    public string CardNumber { get; set; } = null!;
+    public string Expiration { get; set; } = null!;
+    public string Cvv { get; set; } = null!;
+    public int PaymentMethod { get; set; } = 0;
+}
+
+// wrapper classes
+public record CheckoutBasketRequest(BasketCheckoutModel BasketCheckout);
+
+public record CheckoutBasketResponse(bool IsSuccess);
---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string UserName { get; set; } = null!;
+    public List<ShoppingCartItemModel> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
+public class ShoppingCartItemModel
+{
+    public int Quantity { get; set; } = 0;
+    public string Color { get; set; } = null!;
+    public decimal Price { get; set; } = 0;
+    public Guid ProductId { get; set; } = Guid.Empty;
+    public string ProductName { get; set; } = null!;
+}
+
+// wrapper classes
+public record GetBasketResponse(ShoppingCartModel Cart);
+
+public record StoreBasketRequest(ShoppingCartModel Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
