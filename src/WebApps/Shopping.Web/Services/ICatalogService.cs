fc5328fdc307f06766440ee628f1e6500d6bd2a4
Davi Rodriguez <42500271+DaviRP2018@users.noreply.github.com>
2026-01-06 18:36:28 -0300

    feat: add ICatalogService interface for catalog operations
    Defined `ICatalogService` with methods for retrieving products, fetching product details, and filtering by category to standardize catalog-related operations in `Shopping.Web`.

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,10 @@
+﻿using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    Task<GetProductResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+}
